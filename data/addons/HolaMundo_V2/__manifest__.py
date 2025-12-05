4b47a2bbaed341afbb6929d05833c128b5bade2c
Quique <enralopui@alu.edu.gva.es>
2025-12-05 12:13:31 +0100

    creacion de los 3 holamundo

---
+++ data/addons/HolaMundo_V2/__manifest__.py
@@ -0,0 +1,18 @@
+# HolaMundo_V1/__manifest__.py
+# HolaMundo_V2/__manifest__.py
+
+{
+    'name': 'Hola Mundo V2',
+    'version': '2.0',
+    'summary': 'Módulo de ejemplo Hola Mundo V2',
+    'description': 'Este módulo muestra un mensaje básico en Odoo con interfaz completa.',
+    'author': 'Tu Nombre',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hola_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
