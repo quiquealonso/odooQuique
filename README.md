6db06d195c7a37419c76f0feecc821291ef873b5
quiquealonso <enralopui@alu.edu.gva.es>
2026-02-03 16:07:59 +0100

    03/02(16:10)

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 - Definicion de todos los modelos y sus atributos a medias (sin las relaciones)
 - Modificar el ir.model.access.csv para dar permisos de acceso a todos los modelos
 - Añadir 1 vista por defecto, una lista i un menuitem para poder acceder a la app
+
+
+# Descripcion 03/02 tarde en casa
+
+- Editar el nombre de los modelos para que sigan el mismo patron
+- Hacer accesibles mediante vistas y formularios predefinidos todos los modelos
+- Hacer la herencia de res.partner
+- Definir algunas relaciones entre los modelos
--- data/addons/dronify_quique/models/models.py
+++ data/addons/dronify_quique/models/models.py
@@ -1,12 +1,46 @@
 from odoo import models, fields, api
 
 
+class dronify_qap_contactos(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    #Identifica si el contacto es cliente
+    es_cliente = fields.Boolean(string="Es Cliente")
+
+    #Marca clientes premium (activa modo ahorro en vuelos)
+    es_vip = fields.Boolean(string="Es vip")
+
+    #Identifica si el contacto es piloto
+    es_piloto = fields.Boolean(string="Es Piloto")
+
+    #Número de licencia del piloto (obligatorio solo para pilotos)
+    licencia = fields.Char(string="Licencia de Piloto")
+
+    #Lista de drones que el piloto está certificado para operar
+    dron_autorizado_ids = fields.Many2many(
+        'dronify_qap_dron',
+        string="Drones Autorizados",
+    )
+
+    #relacion con los paquetes
+    paquete = fields.One2many(
+        'dronify_qap_paquete',
+        'cliente_id',
+        string="Paquetes Enviados",
+    )
+
+
 class dronify_qap_dron(models.Model):
-    _name = 'dronify_quique.dronify_quique'
+    _name = 'dronify_qap_dron'
     _description = 'dronify_quique.dronify_quique'
 
     #Nombre identificativo del dron
-    name = fields.Char()
+    name = fields.Char(
+        string = "nombre"
+    )
+
+    #Carga máxima en kilogramos(obligatorio)
     capacidad_max = fields.Float(
         required=True
     )
@@ -16,51 +50,32 @@ class dronify_qap_dron(models.Model):
         default=100,
         string="Batería",
     )
+    #esto lo tenemos que limitar con una funcion o en la vista basta??
 
    
     #Estado operativo del dron
     estado = fields.Selection(
         [('disponible', 'Disponible'),
           ('vuelo', 'Vuelo'),
-          ('taller', 'NoTallerche')],
+          ('taller', 'Taller')],
           string='Estado dron',
           default='disponible'
     )
 
     #Pilotos certificados para este dron (relación inversa)
-    #piloto_autorizado_ids = fields.Many2many()
+    piloto_autorizado_ids = fields.Many2many(
+        'res.partner',
+        string="Pilotos Autorizados",
+        domain=[('es_piloto', '=', True)],
+    )
+
+    paquete = fields.One2many(
+        'dronify_qap_paquete',
+        'vuelo_id',
+        string="Paquetes Asignados",
+    )
 
      
-
-class dronify_qap_vuelo(models.Model):
-    _name = 'dronify_qap_vuelo'
-    _description = 'dronify_qap_vuelo'
-
-    #Código único del vuelo
-    codigo = fields.Char(
-        required=True,
-        string="Código Vuelo",
-        readonly=True,
-    )
-
-    name = fields.Char(
-        string="Denominación de la misión",
-       # default=datetime.now().strftime("Vuelo %Y-%m-%d %H:%M:%S"),
-       required=True,
-    )
-
-    #dron asignado
-    #dron_id = fields.Many2one(
-
-    preparado = fields.Boolean(
-        string="Preparado para despegue",
-    )
-
-    realizado = fields.Boolean(
-        string="Vuelo realizado",
-    )
-
-
 class dronify_qap_paquete(models.Model):
     _name = 'dronify_qap_paquete'
     _description = 'dronify_qap_paquete'
@@ -84,29 +99,87 @@ class dronify_qap_paquete(models.Model):
         required=True,
     )
 
-    #Cliente que envía el paquete
-    #cliente_id = fields.Many2one()
+    #Cliente que envía el paquete(al piloto) (obligado)
+    cliente_id = fields.Many2one(
+        'res.partner',
+        string="Cliente Remitente",
+        required=True,
+        domain=[('es_cliente', '=', True)],
+    )
 
-    #Vuelo asignado
-    #vuelo_id = fields.Many2one()
+    #Vuelo asignado (solo lectura)
+    vuelo_id = fields.Many2one(
+        'dronify_qap_vuelo',
+        string="Vuelo Asignado",
+        readonly=True,
+    )
+
+    #Nombre del dron del vuelo(campo Related, solo lectura Solo lectura)
+   # dron_relacionado = fields.related()
        
     #Nombre del dron del vuelo
     #dron_relacionado = fields.Char()
 
 
-# class dronify_qap_contactos(models.Model):
-#     _name = 'res.partner'
-#     _inherit = 'res.partner'
 
-#     #Identifica si el contacto es cliente
-#     es_cliente = fields.Boolean(string="Es Cliente")
+class dronify_qap_vuelo(models.Model):
+    _name = 'dronify_qap_vuelo'
+    _description = 'dronify_qap_vuelo'
 
-#     #Marca clientes premium (activa modo ahorro en vuelos)
-#     es_vip = fields.Boolean(string="Es vip")
+    #Código único del vuelo
+    codigo = fields.Char(
+        required=True,
+        string="Código Vuelo",
+        readonly=True,
+    )
 
-#     #Identifica si el contacto es piloto
-#     es_piloto = fields.Boolean(string="Es Piloto")
+    name = fields.Char(
+        string="Denominación de la misión",
+       # default=datetime.now().strftime("Vuelo %Y-%m-%d %H:%M:%S"),
+       required=True,
+    )
 
-#     #Número de licencia del piloto (obligatorio solo para pilotos)
-#     licencia = fields.Char(string="Licencia de Piloto")
+    #dron asignado
+    dron_id = fields.Many2one(
+        'dronify_qap_dron',
+        string="Dron Asignado",
+        required=True,
+    )
+
+    #piloto asignado
+    piloto_id = fields.Many2one(
+        'res.partner',
+        string="Piloto Asignado",
+        required=True,
+        domain=[('es_piloto', '=', True)],
+    )
+
+    #paquetes a transportar
+    paquetes_ids = fields.One2many(
+        'dronify_qap_paquete',
+        'vuelo_id',
+        string="Paquetes Asignados",
+    )
+
+    #Indica si el vuelo está listo para ejecutarse
+    preparado = fields.Boolean(
+        string="Preparado para despegue",
+    )
+
+    #Indica si el vuelo se ha completado
+    realizado = fields.Boolean(
+        string="Vuelo realizado",
+    )
+
+    #Suma del peso de todos los paquetes asignados (campo computado)
+    #peso_total = fields.computed()
+
+    #Porcentaje de batería que consumirá el vuelo (campo computado)
+    #consumo_estimado = fields.computed()
+
+
+
+
+
+
 
